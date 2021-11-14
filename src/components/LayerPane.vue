c3ec472a9c7cdd959b8ba7a6f9c58805502b7b52
Proladon <Proladon@gmail.com>
2021-11-14 18:48:55 +0800

    stage

---
+++ src/components/LayerPane.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layer-pane">
+    <div>Layer</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayerPane",
+}
+</script>
+
+<style scoped lang="postcss"></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: () => ({
+    curLayer: 0,
+  }),
+  computed: {
+    preLayer() {
+      if (this.curLayer === 0) return 0
+      return this.curLayer - 1
+    },
+    nextLayer() {
+      return this.curLayer + 1
+    },
+  },
+}
+</script>
+
+<style scoped lang="postcss"></style>
