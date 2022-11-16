aa8257ca2224b7143cb1c43297bf60dacf522e50
Proladon <Proladon@gmail.com>
2022-11-16 20:23:24 +0800

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/d3-force-test' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/wordnet.io' : '/',
 }
